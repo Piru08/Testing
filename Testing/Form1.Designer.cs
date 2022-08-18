594843033d17483979805f800cc0b64e1cd837ac
Piru08 <87448452+Piru08@users.noreply.github.com>
2022-08-18 19:40:36 +0300

    Add files
    Add files

---
+++ Testing/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(39, 48);
+            this.textBox1.MaxLength = 17;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите VIN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 148);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "VIN found";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Testing/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+//Перед использованием внести актуальный путь к mdf файлу в App.config/...connectionString=
+namespace Testing
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, string> wmDict;
+        public Dictionary<string, string> labelDict;
+
+        private SqlConnection sqlConnection;
+        public Form1()
+        {
+            Dictions dictions = new Dictions();
+            InitializeComponent();
+            wmDict = dictions.CreateWmDictionary();
+            labelDict = dictions.CreateLabelDictionary();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RequestHistory"].ConnectionString);
+            sqlConnection.Open();
+
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("SQL is connected!");
+            }
+            else
+            {
+                MessageBox.Show("SQL is not connected!" +
+                    "\nНеобходимо указать верный путь к библиотеке RequestHistory.mdf.");
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length < 17)
+            {
+                MessageBox.Show("Некорректное число сомволов VIN-кода!");
+                return;
+            }
+
+            string vin = textBox1.Text.ToUpper();
+            string wmCode = $"{vin[0]}{vin[1]}";
+            string labelCode = $"{vin[2]}";
+            string country, label;
+
+            string commandInsert = "INSERT INTO [TableHist] (Time, Vin) " +
+                $"VALUES ('{DateTime.Now}', '{vin}')";
+
+            if (wmDict.ContainsKey(wmCode))
+            {
+                country = wmDict[wmCode];
+            }
+            else
+            {
+                country = "Не определена";
+            }
+
+            if (labelDict.ContainsKey(labelCode))
+            {
+                label = labelDict[labelCode];
+            }
+            else
+            {
+                label = "Не определена";
+            }
+
+            SqlCommand command = new SqlCommand(commandInsert, sqlConnection);
+            command.ExecuteNonQuery();
+
+
+
+            MessageBox.Show($"Страна производителя: {country}." +
+                $"\nМарка автомобиля: {label}.");
+        }
+        
+    }
+}
